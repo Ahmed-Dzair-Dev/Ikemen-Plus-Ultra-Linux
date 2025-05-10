ebada40ee0286dfbc38dd2a908c47f8401ed9c76
Ahmed-Dzair-Dev <gabrial2zangi@proton.me>
2025-05-10 15:17:25 +0100

    Add files via upload

---
+++ src/lib/dll/alert/alert/build/CMakeFiles/alert.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/alert.dir/alert.cpp.o"
+  "CMakeFiles/alert.dir/alert.cpp.o.d"
+  "lib/libalert.pdb"
+  "lib/libalert.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/alert.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
